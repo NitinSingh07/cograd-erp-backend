08e98c025b7fad190c8e0f058005354a3b29c634
varun__upadhyay <varun.802.vu@gmail.com>
2024-04-05 00:20:14 +0530

    teacher attendance success

--- routes/studentAttendanceRoutes.js
+++ routes/studentAttendanceRoutes.js
@@ -4,11 +4,11 @@ const express = require("express");
 const router = express.Router();
 const { takeAttendance, updateAttendance, getStudentList, getStudentAttendanceByDate, getAllStudentsAttendanceByDate } = require("../controllers/studentAttendanceController");
 
-router.post("/mark/:classTeacherId", takeAttendance);
+router.post("/mark", takeAttendance);
 // router.put("/mark/:classTeacherId", updateAttendance); // Use PUT method for updating
 router.get('/students/:classTeacherId', getStudentList);
 // http://localhost:4000/attendance/attendance/660d9780d4437a2a864c8c02/660d98c9a9792b8374fe794b/2024-04-03T18:30:00.000Z
-router.get('/attendance/:classTeacherId/:studentId/:date', getStudentAttendanceByDate);
-router.get('/attendance/:classTeacherId/:date', getAllStudentsAttendanceByDate);
+router.get('/:classTeacherId/:studentId/:date', getStudentAttendanceByDate);
+router.get('/:classTeacherId/:date', getAllStudentsAttendanceByDate);
 
 module.exports = router;
--- routes/teacherAttendanceRoutes.js
+++ routes/teacherAttendanceRoutes.js
@@ -1,17 +1,20 @@
-// teacherAttendanceRoutes.js
-
 const express = require("express");
 const router = express.Router();
-const { takeTeacherAttendance, getTeacherAttendanceByDate, 
-    getAllTeachersAttendanceByDate,getTeachersBySchool } = require("../controllers/teacherAttendanceController");
-    
-//school name and status in the body to mark attendance 
-// "schoolName":"660d8dc47c38601f49dcfcaf",
-// "status":["a","p"]
+const { 
+    takeTeacherAttendance, 
+    getTeacherAttendanceByDate, 
+    getAllTeachersAttendanceByDate,
+    getTeachersBySchool 
+} = require("../controllers/teacherAttendanceController");
+
+// Route for marking attendance
 router.post("/mark", takeTeacherAttendance);
-//below route is same for teacher also 
-router.get("/get/:schoolId",getTeachersBySchool)
-router.get('/attendance/:schoolName/:teacherId/:date', getTeacherAttendanceByDate);
-router.get('/attendance/:schoolName/:date', getAllTeachersAttendanceByDate);
+
+// Route for getting teachers by school
+router.get("/get/:schoolId", getTeachersBySchool);
+
+// Routes for getting teacher attendance
+router.get('/:schoolName/:teacherId/:date', getTeacherAttendanceByDate);
+router.get('/:schoolName/:date', getAllTeachersAttendanceByDate);
 
 module.exports = router;
