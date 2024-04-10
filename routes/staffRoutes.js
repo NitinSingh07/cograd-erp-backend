d56fc2e5c74727c4733a0c247eaea0d185fc1096
varun__upadhyay <varun.802.vu@gmail.com>
2024-04-10 13:28:23 +0530

    restricted partially staff routes, added jwt authentication in school routes

--- routes/staffRoutes.js
+++ routes/staffRoutes.js
@@ -2,9 +2,10 @@
 const express = require('express');
 const router = express.Router();
 const { addStaffMember, getStaffDetails } = require('../controllers/staffController');
+const { restrictTo } = require('../middleware/auth');
 
 // Route to add a new staff member
 router.post('/', addStaffMember);
-router.get('/:schoolId',getStaffDetails)
+router.get('/:schoolId' , getStaffDetails)
 
 module.exports = router;
