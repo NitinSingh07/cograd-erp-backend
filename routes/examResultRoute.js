ce0e0e4ba89db102a2c607c2a300894ca896c0ea
suraj maheshwari <surajmaheshwari159@gmail.com>
2024-06-01 11:50:59 +0530

    exam rsult controller

--- routes/examResultRoute.js
+++ routes/examResultRoute.js
@@ -3,11 +3,18 @@ const {
   findResultofStudent,
   addExamResult,
   getClassExamResults,
+  editExamResult,
+  deleteExamResult,
 } = require("../controllers/examResultController");
 const router = express.Router();
 
 router.post("/", addExamResult);
 router.get("/:id", findResultofStudent);
 router.get("/classResult/:classId", getClassExamResults);
+router.put("/update/:id", editExamResult);
+router.delete(
+  "/delete/:examResultId/:classTeacherId/:studentId",
+  deleteExamResult
+);
 
 module.exports = router;
