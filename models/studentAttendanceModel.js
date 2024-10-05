1e631b8896f99f7993191554872e9746c1f9c92d
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2024-10-05 19:26:03 +0530

    Add check-in and check-out options to student attendance model

--- models/studentAttendanceModel.js
+++ models/studentAttendanceModel.js
@@ -25,6 +25,14 @@ const studentAttendanceSchema = new mongoose.Schema({
     enum: ["p", "a", "l"],
     default: "a",
   },
+  checkIn: {
+    type: [String], // Array of strings to store multiple check-in options
+    default: [],
+  },
+  checkOut: {
+    type: [String], // Array of strings to store multiple check-out options
+    default: [],
+  },
 });
 
 module.exports = mongoose.model("attendance", studentAttendanceSchema);
